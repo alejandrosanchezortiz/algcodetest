60f76fe4829b8a3e996bca2c447d4d442b817907
a.a.sanchez.ortiz <a.a.sanchez.ortiz@CPX-V1J3XH9CXGA>
2019-08-05 13:22:14 +0200

    Now the CSS Chart is working

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Users
 
 def index(request):
     # Request the context of the request.
@@ -11,10 +12,10 @@ def index(request):
     context = RequestContext(request)
 
     # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    users_list = Users.objects.order_by('user_id')[:10]
+    output = {'users_list': users_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render_to_response('users/index.html', context_dict, context)
+    return render_to_response('users/index.html', output, context)
