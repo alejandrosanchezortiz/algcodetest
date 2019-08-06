fc564cd4feb1d1adbae30d2dbcfc280458972fd5
a.a.sanchez.ortiz <a.a.sanchez.ortiz@CPX-V1J3XH9CXGA>
2019-08-06 13:14:51 +0200

    Home and View_Chart new pages

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('users.urls')),
+    path('view_chart/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
--- users/views.py
+++ users/views.py
@@ -6,20 +6,16 @@ from django.http import HttpResponse
 
 from .models import Users
 
-#def index(request):
-    # Request the context of the request.
+def view_chart(request):
     # The context contains information such as the client's machine details, for example.
-    #context = RequestContext(request)
+    context = RequestContext(request)
 
     # Construct a dictionary to pass to the template engine as its context.
-    #users_list = Users.objects.order_by('user_id')[:10]
-    #output = {'users_list': users_list}
+    users_list = Users.objects.order_by('user_id')[:10]
+    output = {'users_list': users_list}
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    #return render_to_response('users/index.html', output, context)
+    return render_to_response('users/view_chart.html', output, context)
 
 def index(request):
-    title = " Users Connectios - Code Test -"
-    return render(request, 'users/base.html', {'title': title})
+    title = " Users Connections - Code Test -"
+    return render(request, 'users/home.html', {'title': title})
